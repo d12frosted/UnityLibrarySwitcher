86cd50407a4e120d41c76ef16daa15f311cfcebf
d12frosted <d12frosted@icloud.com>
2015-07-05 20:02:12 +0300

    remove unused code

--- UnityLibrarySwitcher/Editor/LibrarySwitcherWindow.cs
+++ UnityLibrarySwitcher/Editor/LibrarySwitcherWindow.cs
@@ -121,37 +121,6 @@ namespace UnityLibrarySwitcher
             return(Size);
         }
 
-        /*
-        public static long DirSize(string sourceDir, bool recurse)
-        {
-            long size = 0;
-            string[] fileEntries = Directory.GetFiles(sourceDir);
-
-            foreach (string fileName in fileEntries)
-            {
-                Interlocked.Add(ref size, (new FileInfo(fileName)).Length);
-            }
-
-            if (recurse)
-            {
-                string[] subdirEntries = Directory.GetDirectories(sourceDir);
-
-                Parallel.For<long>(0, subdirEntries.Length, () => 0, (i, loop, subtotal) =>
-                        {
-                            if ((File.GetAttributes(subdirEntries[i]) & FileAttributes.ReparsePoint) != FileAttributes.ReparsePoint)
-                            {
-                                subtotal += DirSize(subdirEntries[i], true);
-                                return subtotal;
-                            }
-                            return 0;
-                        },
-                                   (x) => Interlocked.Add(ref size, x)
-                                   );
-            }
-            return size;
-        }
-        */
-
         #endregion
     }
 }
